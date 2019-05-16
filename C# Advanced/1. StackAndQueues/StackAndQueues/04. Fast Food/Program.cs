c776986163cc3d434d19f177207b211a7ccb67f2
ivaylo-apostolov <ivaylo.apostolov@gmail.com>
2019-05-16 19:05:11 +0300

    no message

---
+++ C# Advanced/1. StackAndQueues/StackAndQueues/04. Fast Food/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityOfFood = int.Parse(Console.ReadLine());
+
+            var inputOrders = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var orders = new Queue<int>(inputOrders);
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Count > 0)
+            {
+                if (quantityOfFood >= orders.Peek())
+                {
+                    quantityOfFood -= orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+        }
+    }
+}
