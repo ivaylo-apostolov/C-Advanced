8a91f0cd76f362e9f907a5a466e08c7e6a74b9ba
ivaylo-apostolov <ivaylo.apostolov@gmail.com>
2019-05-16 10:07:55 +0300

    Problem 2 and 3 solved

---
+++ C# Advanced/1. StackAndQueues/StackAndQueues/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputIntegers = Console.ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            int numbersToRemove = inputIntegers[1];
+            int numberToContains = inputIntegers[2];
+
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var queue = new Queue<int>(input);
+
+            for (int i = 0; i < numbersToRemove; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(numberToContains))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count > 0)
+            {
+                Console.WriteLine(queue.Min());
+            }
+            else if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
---
+++ C# Advanced/1. StackAndQueues/StackAndQueues/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+
+            int inputNumbers = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputNumbers; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var command = input[0];
+
+                if (command == 1)
+                {
+                    int numberToPush = input[1];
+                    stack.Push(numberToPush);
+                }
+                else if (command == 2 && stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+                else if (command == 3 && stack.Count > 0)
+                {
+                    Console.WriteLine(stack.Max());
+                }
+                else if (command == 4 && stack.Count > 0)
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
