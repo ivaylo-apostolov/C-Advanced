7833d694c6dd0491c45864c6abf9fb949e734f86
ivaylo-apostolov <ivaylo.apostolov@gmail.com>
2019-05-17 10:05:53 +0300

    Problems 5,6 solved

---
+++ C# Advanced/1. StackAndQueues/StackAndQueues/05. Fashion Boutique/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputIntegers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse);
+
+            var stack = new Stack<int>(inputIntegers);
+
+            int capacity = int.Parse(Console.ReadLine());
+            int startCapacity = capacity;
+            int totalRacks = 0;
+
+            if (stack.Min() > 0)
+            {
+                totalRacks = 1;
+            }
+
+            while (stack.Count > 0)
+            {
+                if (capacity > stack.Peek())
+                {
+                    capacity -= stack.Pop();
+                }
+                else if (capacity == stack.Peek())
+                {
+                    if (stack.Count > 1)
+                    {
+                        stack.Pop();
+                        totalRacks++;
+                        capacity = startCapacity;
+                    }
+                }
+                else if (capacity < stack.Peek())
+                {
+                    totalRacks++;
+                    capacity = startCapacity;
+                    continue;
+                }
+            }
+
+            Console.WriteLine(totalRacks);
+        }
+    }
+}
---
+++ C# Advanced/1. StackAndQueues/StackAndQueues/06. Auto Repair and Service/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Auto_Repair_and_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputVehicals = Console.ReadLine()
+                .Split();
+
+            var service = new Queue<string>(inputVehicals);
+            var servedCars = new Stack<string>();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var tokens = input.Split("-").ToArray();
+                string command = tokens[0];
+
+                if (command == "Service" && service.Count > 0)
+                {
+                    servedCars.Push(service.Dequeue());
+                    Console.WriteLine($"Vehicle {servedCars.Peek()} got served.");
+                }
+                else if (command == "CarInfo")
+                {
+                    string carToCheck = tokens[1];
+
+                    if (service.Contains(carToCheck))
+                    {
+                        Console.WriteLine("Still waiting for service.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Served.");
+                    }
+                }
+                else if (command == "History")
+                {
+                    Console.WriteLine(string.Join(", ", servedCars));
+                }
+            }
+
+            if (service.Count > 0)
+            {
+                Console.WriteLine($"Vehicles for service: {string.Join(", ", service)}");
+            }
+
+            if (servedCars.Count > 0)
+            {
+                Console.WriteLine($"Served vehicles: {string.Join(", ", servedCars)}");
+            }
+        }
+    }
+}
