fa04f4353ec8487780c228de52ccd94165fac10b
ivaylo-apostolov <ivaylo.apostolov@gmail.com>
2019-06-04 09:57:07 +0300

    5. Functional programing P12 solver 6. Defining Classes adding documents

---
+++ C# Advanced/5. Functional Programing/FunctionalProgramintExercise/12. TriFunction/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputCharacterLength = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            foreach (var name in names)
+            {
+                if (IsThisNameLongerThan(inputCharacterLength, name, SymbolsAndName))
+                {
+                    Console.WriteLine(name);
+                    break;
+                }
+            }
+        }
+
+        public static bool IsThisNameLongerThan(int num, string name, Func<int, string, bool> operation)
+        {
+            return operation(num, name);
+        }
+        public static bool SymbolsAndName(int number, string name)
+        {
+            int count = 0;
+
+            foreach (var symbol in name)
+            {
+                count += symbol;
+            }
+
+            if (count >= number)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Func<int, string, bool> SymbolsAndName = (number, name) =>
+        //{
+        //    int count = 0;
+
+        //    foreach (var symbol in name)
+        //    {
+        //        count += symbol;
+        //    }
+
+        //    if (count >= number)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //};
+    }
+}
