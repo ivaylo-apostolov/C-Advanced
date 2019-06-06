6dbeb1613941d9df934f9a814e1b542a2cde669e
Ivaylo Apostolov <ivaylo.apostolov@gmail.com>
2019-06-07 00:27:40 +0300

    Defining classes p1-4

---
+++ C# Advanced/6. Defining Classes/Defining Classes Exercise/001. Define a Class Person/Family.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> People;
+
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+
+        public List<Person> Sort(List<Person> list)
+        {
+            return list.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+        }
+    }
+}
---
+++ C# Advanced/6. Defining Classes/Defining Classes Exercise/001. Define a Class Person/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                var name = tokens[0];
+                var age = int.Parse(tokens[1]);
+
+                var member = new Person(name, age);                
+
+                family.AddMember(member);
+            }
+
+            foreach (var name in family.Sort(family.People))
+            {
+                Console.WriteLine(name.ToString());
+            }
+        }
+    }
+}
