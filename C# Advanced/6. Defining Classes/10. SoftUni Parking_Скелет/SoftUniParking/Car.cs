a22ebc508cb7692f1260becc2aac41f3bb3ccc84
Ivaylo Apostolov <ivaylo.apostolov@gmail.com>
2019-06-12 00:20:15 +0300

    Defining classes P10 - solved, ready for testing

---
+++ C# Advanced/6. Defining Classes/10. SoftUni Parking_Скелет/SoftUniParking/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        private string make;
+
+        private string model;
+
+        private int horsePower;
+
+        private string registrationNumber;
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public string Make { get => make; set => make = value; }
+
+        public string Model { get => model; set => model = value; }
+
+        public int HorsePower { get => horsePower; set => horsePower = value; }
+
+        public string RegistrationNumber { get => registrationNumber; set => registrationNumber = value; }
+    }
+}
---
+++ C# Advanced/6. Defining Classes/10. SoftUni Parking_Скелет/SoftUniParking/Parking.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private int capacity;
+        private List<Car> cars;
+
+        public Parking(int capacity)
+        {
+            this.cars = new List<Car>();
+            this.capacity = capacity;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return cars.Capacity;
+            }
+        }
+
+        public string AddCar(Car car)
+        {
+            Car checkCar = cars.Where(c => c.RegistrationNumber == car.RegistrationNumber).FirstOrDefault();
+
+            if (checkCar != null)
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (cars.Count == capacity)
+            {
+                return "Parking is full!";
+            }
+
+            cars.Add(car);
+
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            Car checkCar = cars.Where(c => c.RegistrationNumber == registrationNumber).FirstOrDefault();
+
+            if (checkCar != null)
+            {
+                cars.Remove(checkCar);
+                return $"Successfully removed {registrationNumber}";
+            }
+
+            return "Car with that registration number, doesn't exist!";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            Car checkCar = cars.Where(c => c.RegistrationNumber == registrationNumber).FirstOrDefault();
+            
+            return checkCar;
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var registrationNumber in registrationNumbers)
+            {
+                Car checkCar = cars.Where(c => c.RegistrationNumber == registrationNumber).FirstOrDefault();
+
+                if (checkCar != null)
+                {
+                    cars.Remove(checkCar);
+                }
+            }
+        }
+    }
+}
